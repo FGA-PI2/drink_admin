1f05356a23d88427472c9f29bba407a5c3e8a6f0
Rafael Akiyoshi <rafah94@hotmail.com>
2017-10-23 16:45:59 -0200

    Upgrade Login UX

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,58 @@
 <template>
     <div id="login">
-        <h2>SunBar Admin</h2>
-        <Input class="center" v-model="email" placeholder="E-mail..." style="width: 400px"></Input>
+        <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
+        <FormItem prop="user">
+            <Input type="text" v-model="formInline.user" placeholder="Email..." style="width: 300px">
+                <Icon type="ios-person-outline" slot="prepend"></Icon>
+            </Input>
+        </FormItem>
+            </br>
+        <FormItem prop="password">
+            <Input type="password" v-model="formInline.password" placeholder="Senha..." style="width: 300px">
+                <Icon type="ios-locked-outline" slot="prepend"></Icon>
+            </Input>
+        </FormItem>
         </br>
-        </br>
-        </br>
-        <Input class="center" v-model="senha" placeholder="Senha..." style="width: 400px" type="password"></Input>
-        </br>
-        </br>
-        </br>
-         <Button @click="tryLogin()" style="width: 400px" type="success">Login</Button>
+        <FormItem>
+            <Button type="primary" @click="handleSubmit('formInline')" style="width: 300px">Logar</Button>
+        </FormItem>
+    </Form>
+         <Spin size="large" fix v-if="spinShow"></Spin>
     </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      email: '',
-      senha: ''
+      formInline: {
+        user: '',
+        password: ''
+      },
+      ruleInline: {
+        user: [{ required: true, message: 'Campo Obrigatório', trigger: 'blur' }],
+        password: [
+          { required: true, message: 'Campo Obrigatório', trigger: 'blur' },
+          { type: 'string', min: 6, message: 'O comprimento da senha não pode ser inferior a 6 dígitos', trigger: 'blur' }
+        ]
+      },
+      spinShow: false
     }
   },
   methods: {
     tryLogin () {
-      alert(this.email, this.senha)
+      console.log(this.formInline.user)
+      console.log(this.formInline.password)
+      this.spinShow = false
+    },
+    handleSubmit (name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          this.spinShow = true
+          this.tryLogin()
+        } else {
+          this.$Message.error('Falha na validação do formulário!')
+        }
+      })
     }
   }
 }
