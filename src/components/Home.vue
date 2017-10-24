965fa3db5092865880aadf3315bb431696c9b59e
Rafael Akiyoshi <rafah94@hotmail.com>
2017-10-24 19:03:53 -0200

    created Users log and orders history

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -28,12 +28,14 @@
                 <div class="layout-content">
                     <div class="layout-content-main">
                       <div v-if="option === '1'">
-                        <h2>Bem-Vindo {{user.nome}}!</h2>
+                        <h1>Bem-Vindo {{user.nome}}!</h1>
+                        </br>
+                        <h2>Bebidas da Máquina</h2>                        
                         <Row>
                           <Col span="8">
                               <Card>
                                 <p slot="title">Posição #1 {{drinks[0].nome}}</p>
-                                  <i-circle :percent="drinksSize[0].size">
+                                  <i-circle :percent="drinksSize[0].size" :stroke-color="drinksSize[0].color">
                                     <span class="demo-Circle-inner" style="font-size:24px">{{drinksSize[0].size}}%</span>
                                 </i-circle>
                               </Card>
@@ -41,7 +43,7 @@
                           <Col span="8">
                               <Card>
                                   <p slot="title">Posição #2 {{drinks[1].nome}}</p>
-                                  <i-circle :percent="drinksSize[1].size" stroke-color="#4db8ff">
+                                  <i-circle :percent="drinksSize[1].size" :stroke-color="drinksSize[1].color">
                                     <span class="demo-Circle-inner" style="font-size:24px">{{drinksSize[1].size}}%</span>
                                 </i-circle>
                               </Card>
@@ -49,33 +51,62 @@
                           <Col span="8">
                               <Card dis-hover>
                                   <p slot="title">Posição #3 {{drinks[2].nome}}</p>
-                                  <i-circle :percent="drinksSize[2].size">
+                                  <i-circle :percent="drinksSize[2].size" :stroke-color="drinksSize[2].color">
                                     <span class="demo-Circle-inner" style="font-size:24px">{{drinksSize[2].size}}%</span>
                                 </i-circle>
                               </Card>
                           </Col>
                       </Row>
+                      </br></br>
+                      <h2>Histórico de Compras</h2>
+                      <Table :columns="columnsOrders" :data="dataOrders"></Table>
                     </div>
                     <div v-if="option === '2'">
                       <Row>
-                        <Col span="11">
                             <Card>
                                 <p slot="title">Bebidas</p>
                                 <Table :columns="columns1" :data="data1"></Table>
+                                <Button type="success" @click="modalNewBebida = true" long>Cadastrar Nova Bebida</Button>
+                                <Modal v-model="modal2" width="360">
+                                  <p slot="header" style="color:#f60;text-align:center">
+                                      <Icon type="information-circled"></Icon>
+                                      <span>Atenção!</span>
+                                  </p>
+                                  <div style="text-align:center">
+                                      <p>Você está prestes a excluir uma bebida do seu banco de dados!</p>
+                                      <p>Tem certeza que deseja excluir?</p>
+                                  </div>
+                                  <div slot="footer">
+                                      <Button type="error" size="large" long :loading="modal_loading" @click="del">Excluir</Button>
+                                  </div>
+                                  <Modal
+                                    v-model="modalNewBebida"
+                                    title="Cadastrar nova Bebida"
+                                    ok-text="Cadastrar"
+                                    cancel-text="Cancelar">
+                                    <Form label-position="left" :label-width="100">
+                                      <FormItem label="Nome">
+                                          <Input></Input>
+                                      </FormItem>
+                                      <FormItem label="Preço">
+                                          <Input ></Input>
+                                      </FormItem>
+                                      <FormItem label="Volume">
+                                          <Input ></Input>
+                                      </FormItem>
+                                  </Form>
+                                </Modal>
+                              </Modal>
                             </Card>
-                        </Col>
-                        <Col span="11" offset="2">
                             <Card dis-hover>
                                 <p slot="title">Drinks</p>
-                                <p>卡片内容</p>
-                                <p>卡片内容</p>
-                                <p>卡片内容</p>
+                                <Table :columns="columns2" :data="data2"></Table>
+                                <Button type="success" @click="modalNewBebida = true" long>Cadastrar Novo Drinks</Button>
                             </Card>
-                        </Col>
                     </Row>
                     </div>
                     <div v-if="option === '3'">
-                      <h2>USERS ADMIN</h2>
+                      <Table :columns="columnsUsers" :data="dataUsers"></Table>
                     </div>
                     </div>
                 </div>
@@ -92,15 +123,35 @@ import oboe from 'oboe'
 export default {
   data () {
     return {
+      orders: null,
+      modalNewBebida: false,
+      modal2: false,
       user: null,
       spanLeft: 5,
       spanRight: 19,
       drinks: null,
+      cardapio: null,
       option: '1',
       drinksSize: [
-        {size: 0},
-        {size: 0},
-        {size: 0}
+        {size: 0, color: ''},
+        {size: 0, color: ''},
+        {size: 0, color: ''}
+      ],
+      dataUsers: [],
+      columnsUsers: [
+        {title: 'Nome', key: 'first_name'},
+        {title: 'Email', key: 'email'},
+        {title: 'Data de Nascimento', key: 'data_nascimento'},
+        {title: 'Créditos', key: 'creditos'},
+        {title: 'Super Usuário', key: 'is_superuser'}
+      ],
+      dataOrders: [],
+      columnsOrders: [
+        {title: 'Nome', key: 'nome'},
+        {title: 'Data', key: 'data_compra'},
+        {title: 'Usuario', key: 'usuario'},
+        {title: 'Preco', key: 'preco'},
+        {title: 'Valido', key: 'is_valid'}
       ],
       columns1: [
         {title: 'Nome', key: 'nome'},
@@ -136,7 +187,7 @@ export default {
                 },
                 on: {
                   click: () => {
-                    alert(params.index)
+                    this.modal2 = true
                   }
                 }
               }, 'Excluir')
@@ -144,15 +195,64 @@ export default {
           }
         }
       ],
-      data1: []
+      data1: [],
+      columns2: [
+        {title: 'Nome', key: 'nome'},
+        {title: 'Descricao', key: 'descricao'},
+        {title: 'Preco', key: 'preco'},
+        {title: 'Volume', key: 'volume'},
+        {title: 'Proporção', key: 'proporcao'},
+        {
+          title: 'Ações',
+          key: 'action',
+          width: 150,
+          align: 'center',
+          render: (h, params) => {
+            return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      alert(params.index)
+                    }
+                  }
+                }, 'Editar'),
+              h('Button', {
+                props: {
+                  type: 'error',
+                  size: 'small'
+                },
+                on: {
+                  click: () => {
+                    this.modal2 = true
+                  }
+                }
+              }, 'Excluir')
+            ])
+          }
+        }
+      ],
+      data2: [],
     }
   },
   methods: {
     setGraphics () {
-      for (var i = 0; i <= 80; i++) {
-        this.drinksSize[0].size++
-        this.drinksSize[1].size++
-        this.drinksSize[2].size++
+      for(let x in this.drinksSize){
+        this.drinksSize[x].size = (this.drinks[x].remaining_quantity * 100) / this.drinks[x].volume
+        console.log('the size is: ',this.drinksSize[x].size)
+        if(this.drinksSize[x].size <= 10){
+          this.drinksSize[x].color = '#ff5500'
+        } else if (this.drinksSize[x].size > 10 && this.drinksSize[x].size <= 35){
+          this.drinksSize[x].color = '#ffd480'
+        } else {
+          this.drinksSize[x].color = '#80d4ff'
+        }
       }
     },
     toggleClick () {
@@ -169,6 +269,39 @@ export default {
     console.log('INICIOU O COMPUTED')
     this.user = this.$store.getters.returnUser
     oboe({
+      url: `http://dev-pi2-api.herokuapp.com/users/`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': this.user.token
+      }
+    })
+    .done((res) => {
+      this.dataUsers = res
+    })
+    .fail((errorReport) => {
+      console.log(errorReport)
+    })
+    oboe({
+      url: `http://dev-pi2-api.herokuapp.com/compra/`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': this.user.token
+      }
+    })
+    .done((res) => {
+      console.log('huhuhuhu', res)
+      this.dataOrders = res
+      for(let x in this.dataOrders){
+        this.dataOrders[x]['is_valid'] = this.dataOrders[x].qr_code.is_valid
+      }
+      console.log('ORDES', this.dataOrders)
+    })
+    .fail((errorReport) => {
+      console.log(errorReport)
+    })
+    oboe({
       url: `http://dev-pi2-api.herokuapp.com/bebida/`,
       method: 'GET',
       headers: {
@@ -181,6 +314,21 @@ export default {
       this.drinks = res
       this.data1 = res
       this.setGraphics()
+    })
+    .fail((errorReport) => {
+      console.log(errorReport)
+    })
+    oboe({
+      url: `http://dev-pi2-api.herokuapp.com/drink/`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': this.user.token
+      }
+    })
+    .done((res) => {
+      this.cardapio = res
+      this.data2 = res
     })
     .fail((errorReport) => {
       console.log(errorReport)
